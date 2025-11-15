33159aad9d71292657963270e17804949e971dfc
Trien Hong <kientrienhong@gmail.com>
2025-11-15 12:50:19 +0700

    remov unused code

--- app/src/main/java/com/timeskip/ezlink/features/tag/TagNavigation.kt
+++ app/src/main/java/com/timeskip/ezlink/features/tag/TagNavigation.kt
@@ -3,17 +3,12 @@ package com.timeskip.ezlink.features.tag
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.timeskip.ezlink.features.link.navigateToLink
 import com.timeskip.ezlink.features.tag.view.TagScreen
 import com.timeskip.ezlink.navigation.TagListDestination
 import com.timeskip.ezlink.navigation.TagNavigation
-
-fun NavController.navigateToHomeTag(navOptions: NavOptions? = null) {
-    this.navigate(route = TagListDestination, navOptions = navOptions)
-}
 
 fun NavGraphBuilder.tagGraph(navController: NavController, modifier: Modifier) {
     navigation<TagNavigation>(TagListDestination) {
